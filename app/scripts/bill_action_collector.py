e24045928e34b84da468cc6780fe1749b3cbc226
Nick Cleary <nicholascleary01@gmail.com>
2021-11-14 15:36:23 -0500

    updated script

--- app/scripts/bill_action_collector.py
+++ app/scripts/bill_action_collector.py
@@ -18,14 +18,16 @@ def bill_action_collector():
     for bill in bills:
         print('Congress: {0}. Slug: {1}'.format(str(bill.congress), str(bill.bill_slug)))
         result = api.get_bill_activity(bill.bill_slug, bill.congress)
-        for version in result['data']['results'][0]['versions']:
-            version['bill'] = bill.bill_id
-            instance, created = get_or_create(session, BillVersion, bill=version['bill'], congressdotgov_url=version['congressdotgov_url'], defaults=version)
-            session.commit()
-        for action in result['data']['results'][0]['actions']:
-            action['order'] = action['id']
-            action.pop('id')
-            action['bill'] = bill.bill_id
-            instance, created = get_or_create(session, BillAction, bill=action['bill'], order=action['order'], defaults=action)
-            session.commit()
+        if result['data']['results'][0]['versions'] is not None:
+            for version in result['data']['results'][0]['versions']:
+                version['bill'] = bill.bill_id
+                instance, created = get_or_create(session, BillVersion, bill=version['bill'], congressdotgov_url=version['congressdotgov_url'], defaults=version)
+                session.commit()
+        if result['data']['results'][0]['actions'] is not None:
+            for action in result['data']['results'][0]['actions']:
+                action['order'] = action['id']
+                action.pop('id')
+                action['bill'] = bill.bill_id
+                instance, created = get_or_create(session, BillAction, bill=action['bill'], order=action['order'], defaults=action)
+                session.commit()
         time.sleep(17.5)
