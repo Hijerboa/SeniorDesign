ec09ae26fa5013d426942af61064804ee164796b
Ian Wu <ianwu1099@gmail.com>
2021-11-15 13:58:17 -0500

    added runner to gitignore

---
+++ app/runner_example.py.py
@@ -0,0 +1,29 @@
+from util.politician_info_puller import get_congress_members
+#from util.twitter_api_puller import get_tweets
+
+# On mac include this with each script:
+#    import pymysql
+#    pymysql.install_as_MySQLdb()
+
+"""for i in range(80, 118):
+    get_congress_members(i, 'senate')
+    if i > 101:
+        get_congress_members(i, 'house')
+#get_tweets('impeach biden')
+get_tweets('H.Res.57')
+get_tweets('abuse of power biden')
+get_tweets('biden high crimes')
+get_tweets('impeach joe biden')
+get_tweets('criminal biden')"""
+
+#from scripts.TestBillCollector import do_things
+#do_things()
+
+from scripts.bill_action_collector import bill_action_collector
+bill_action_collector()
+
+#from scripts.congress_info_puller import get_congress_members
+#get_congress_members()
+
+#from scripts.congress_info_puller import get_congress_members
+#get_congress_members()
