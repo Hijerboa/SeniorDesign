1037136f432ce7870efb083a1c26a0cf15081b56
Ian Wu <ianwu1099@gmail.com>
2021-11-15 14:12:12 -0500

    Added keyword extraction directory, gitignore add

---
+++ keyword_extraction/utility.py
@@ -0,0 +1,21 @@
+# imports
+import json
+from collections import Counter
+
+stopwords = {'' ,'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'} 
+
+def remove_stopwords(in_string):
+    out_string = ""
+    split_string = in_string.split(' ')
+    for word in split_string:
+        if word not in stopwords:
+            out_string += word + " "
+    return out_string
+
+def get_word_counts(in_string):
+    split_string = in_string.split(' ')
+    counts = Counter(split_string)
+    print(counts)
+
+def load_json_from_file(filepath):
+    return json.load(open(filepath), strict=False)
