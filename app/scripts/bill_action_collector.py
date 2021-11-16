fcab9de6c2a7192ff10f245799c349a995b3b40a
Nick Cleary <nicholascleary01@gmail.com>
2021-11-16 13:50:28 -0500

    Updates to script

--- app/scripts/bill_action_collector.py
+++ app/scripts/bill_action_collector.py
@@ -1,5 +1,5 @@
 from util.cred_handler import get_secret
-from apis.propublica_api import ProPublicaAPI
+from apis.propublica_api import ProPublicaAPI, PropublicaAPIError
 from db.models import Bill, BillAction, BillVersion
 from db.db_utils import get_or_create, create_single_object
 from db.database_connection import initialize, create_session
@@ -17,17 +17,21 @@ def bill_action_collector():
     num = 0
     for bill in bills:
         print('Congress: {0}. Slug: {1}'.format(str(bill.congress), str(bill.bill_slug)))
-        result = api.get_bill_activity(bill.bill_slug, bill.congress)
-        if result['data']['results'][0]['versions'] is not None:
-            for version in result['data']['results'][0]['versions']:
-                version['bill'] = bill.bill_id
-                instance, created = get_or_create(session, BillVersion, bill=version['bill'], congressdotgov_url=version['congressdotgov_url'], defaults=version)
-                session.commit()
-        if result['data']['results'][0]['actions'] is not None:
-            for action in result['data']['results'][0]['actions']:
-                action['order'] = action['id']
-                action.pop('id')
-                action['bill'] = bill.bill_id
-                instance, created = get_or_create(session, BillAction, bill=action['bill'], order=action['order'], defaults=action)
-                session.commit()
+        try:
+            result = api.get_bill_activity(bill.bill_slug, bill.congress)
+            if result['data']['results'][0]['versions'] is not None:
+                for version in result['data']['results'][0]['versions']:
+                    version['bill'] = bill.bill_id
+                    instance, created = get_or_create(session, BillVersion, bill=version['bill'], congressdotgov_url=version['congressdotgov_url'], defaults=version)
+                    session.commit()
+            if result['data']['results'][0]['actions'] is not None:
+                for action in result['data']['results'][0]['actions']:
+                    action['order'] = action['id']
+                    action.pop('id')
+                    action['bill'] = bill.bill_id
+                    instance, created = get_or_create(session, BillAction, bill=action['bill'], order=action['order'], defaults=action)
+                    session.commit()
+        except ProPublicaAPI:
+            time.sleep(600)
+            pass
         time.sleep(17.5)
