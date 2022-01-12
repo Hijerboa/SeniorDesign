ff0c5640c25780ecd07cf6db95d1e8273b3187e8
Ian Wu <ianwu1099@gmail.com>
2022-01-11 19:39:12 -0500

    remove punctuation and fixed added space issue

--- app/machine_learning/keyword_extraction/get_keywords.py
+++ app/machine_learning/keyword_extraction/get_keywords.py
@@ -1,5 +1,6 @@
 # imports
 from functools import reduce
+from string import punctuation
 from numpy.core.numeric import full
 import yake
 from keybert import KeyBERT
@@ -7,7 +8,6 @@ from keybert import KeyBERT
 # list of keywords to exclude - WIP
 bad_keywords = ["united states", "act", "section", "united", "states", "united states of america", "secretary", "federal", "federal government", "government", "congressional", "congress", "bill", "bills", "congress", "senate", "america", "state", "agency", "federal agency", "military", "spending", "healthcare"]
 stopwords = ['', 'a', 'about', 'above', 'after', 'again', 'against', 'ain', 'all', 'am', 'an', 'and', 'any', 'are', 'aren', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can', 'couldn', "couldn't", 'd', 'did', 'didn', "didn't", 'do', 'does', 'doesn', "doesn't", 'doing', 'don', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn', "hadn't", 'has', 'hasn', "hasn't", 'have', 'haven', "haven't", 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'isn', "isn't", 'it', "it's", 'its', 'itself', 'just', 'll', 'm', 'ma', 'me', 'mightn', "mightn't", 'more', 'most', 'mustn', "mustn't", 'my', 'myself', 'needn', "needn't", 'no', 'nor', 'not', 'now', 'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 're', 's', 'same', 'shan', "shan't", 'she', "she's", 'should', "should've", 'shouldn', "shouldn't", 'so', 'some', 'such', 't', 'than', 'that', "that'll", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 've', 'very', 'was', 'wasn', "wasn't", 'we', 'were', 'weren', "weren't", 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'won', "won't", 'wouldn', "wouldn't", 'y', 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves']
-
 
 # YAKE keyword extraction parameters
 language = "en"
@@ -27,6 +27,13 @@ def remove_stopwords(in_string):
     for word in split_string:
         if word not in stopwords:
             out_string += word + " "
+    return out_string[:-1]
+
+
+def remove_punc(in_string):
+    out_string = in_string
+    for punc_mark in punctuation:
+        out_string = in_string.replace(punc_mark, "")
     return out_string
 
 
@@ -42,6 +49,9 @@ def cleanup(my_list: list):
 
     # remove stopwords from selected keywords/phrases
     my_list = list(map(lambda x: remove_stopwords(x), my_list))
+
+    # remove punctuation from keywords
+    my_list = list(map(lambda x: remove_punc(x), my_list))
 
     return my_list[:10] if len(my_list) > 9 else my_list
 
