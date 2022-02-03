0bb0ea7bbfc08444477729e6adb245be7a1b2c0b
Nick Cleary <nicholascleary01@gmail.com>
2022-02-02 20:34:08 -0500

    updates to cron

--- app/cron_runner.py
+++ app/cron_runner.py
@@ -9,12 +9,9 @@ from importlib.util import find_spec
 package_dir = os.path.join(str(Path(__file__).resolve().parent), 'cron_scripts')
 module_name = sys.argv[1]
 if os.path.isdir(os.path.join(package_dir, module_name)):
-    try:
-        # check if it's a python module
-        if find_spec(f'cron_scripts.{module_name}.runner'):
-            module = import_module(f'cron_scripts.{module_name}.runner')
-            module.run()
-    except:
-        print(f'Could not load module {module_name}')
+    # check if it's a python module
+    if find_spec(f'cron_scripts.{module_name}.runner'):
+        module = import_module(f'cron_scripts.{module_name}.runner')
+        module.run()
 else:
     print(f'Could not find module {module_name}')
