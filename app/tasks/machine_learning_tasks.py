32215b00e29208705fa47f7fb071db74cdad26dc
Nick Cleary <nicholascleary01@gmail.com>
2022-02-06 17:39:33 +0000

    Updated tasks to fix errors

--- app/tasks/machine_learning_tasks.py
+++ app/tasks/machine_learning_tasks.py
@@ -8,7 +8,11 @@ from machine_learning.keyword_extraction.get_keywords import get_keywords
 def keyword_extraction_by_bill(id: str):
     session = create_session()
     bill_object: Bill = get_single_object(session, Bill, bill_id = id)
-    sum_keywords, title_keywords = get_keywords(bill_object)
+    try:
+        sum_keywords, title_keywords = get_keywords(bill_object)
+    # occcasionally you run into a one off error on the first few bills processed by a worker. This fixes it
+    except AttributeError:
+        sum_keywords, title_keywords = get_keywords(bill_object)
     for word in sum_keywords:
         object, created = get_or_create(session, SearchPhrase, search_phrase=word, type=phrase_types.summary)
         bill_object.keywords.append(object)
