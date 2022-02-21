10c7d00892db4d2fa5bd7b13fe749fe95e817433
Ian Wu <ianwu1099@gmail.com>
2022-02-21 18:01:24 -0500

    some data ranglin

--- app/machine_learning/sentiment_analysis/check_vader_data.py
+++ app/machine_learning/sentiment_analysis/check_vader_data.py
@@ -6,3 +6,13 @@ def validate_data():
         if line[0] == '1':
             print('BAD')
         line = f.readline()
+
+def fix_data():
+    f = open('app/machine_learning/sentiment_analysis/vader_scored_tweets.csv', 'r', encoding='utf-8', errors='replace')
+    fd = open('app/machine_learning/sentiment_analysis/clean.csv', 'w', encoding='utf-8', errors='replace')
+
+    line = f.readline()
+    while line != '':
+        fd.write(line)
+        line = f.readline()
+
