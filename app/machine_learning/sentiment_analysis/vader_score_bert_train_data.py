99fa042cbc10acd6862db9e8624d496f8a0a5ac8
Ian Wu <ianwu1099@gmail.com>
2022-02-21 17:10:22 -0500

    another one line commit

--- app/machine_learning/sentiment_analysis/vader_score_bert_train_data.py
+++ app/machine_learning/sentiment_analysis/vader_score_bert_train_data.py
@@ -21,7 +21,6 @@ def generate_data():
     session = create_session()
     tweets = session.query(Tweet).order_by(func.random()).limit(NUM_TWEETS).all()
     start = datetime.now()
-    text = [t.text for t in tweets]
     for tweet in tweets:
         text = tweet.text.encode('utf-8').replace('\n', '').replace(',', '')
         score = vader_score(text)
