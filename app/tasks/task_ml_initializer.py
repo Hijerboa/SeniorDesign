126139598a8306d214fe115f1fb53a253047bcde
Nick Cleary <nicholascleary01@gmail.com>
2022-02-24 21:06:20 -0500

    updates

--- app/tasks/task_ml_initializer.py
+++ app/tasks/task_ml_initializer.py
@@ -8,7 +8,7 @@ from util.cred_handler import get_secret
 from db.database_connection import initialize
 from util.cred_handler import get_secret
 
-BROKER_URL = f'amqp://{get_secret("RABBITMQ_USER")}:{get_secret("RABBITMQ_PASS")}@{get_secret("RABBITMQ_HOST")}//'
+BROKER_URL = f'amqp://{get_secret("RABBITMQ_USER")}:{get_secret("RABBITMQ_PASS")}@{get_secret("RABBITMQ_HOST")}:{get_secret("RABBITMQ_PORT")}//'
 
 # Specifies task routes
 task_routes = {
