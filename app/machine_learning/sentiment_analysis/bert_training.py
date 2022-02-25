f03d1ee7d18ea657aed742cf606d3e7c44e201d9
AidanZap <a.zapotechne@vikes.csuohio.edu>
2022-02-25 12:03:49 -0500

    refactors before testing on desktop

--- app/machine_learning/sentiment_analysis/bert_training.py
+++ app/machine_learning/sentiment_analysis/bert_training.py
@@ -1,6 +1,7 @@
 import os, time
 import re
 from tkinter import Y
+from typing import List
 import numpy as np
 import pandas as pd
 from torch import cuda
@@ -14,9 +15,14 @@ import logging
 from transformers import BertTokenizer, TFBertForSequenceClassification
 from transformers import InputExample, InputFeatures
 
-# SequenceClassification - BERT with extra layers at the end for sentiment scoring
-# tf.debugging.set_log_device_placement(True)
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+class Config:
+    def __init__(self, model_file: str, csv_file: str, csv_size: int, dataset_columns: List[str], test_labels: List[int]):
+        self.model_file = model_file
+        self.csv_file = csv_file
+        self.csv_size = csv_size
+        self.dataset_columns = dataset_columns
+        self.test_labels = test_labels
+
 # Tokenizer - preprocessing, prepares inputs for the model
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
@@ -32,6 +38,7 @@ MODEL_NAME = "TRAINED_MODEL_TEST"
 INPUT_FEATURES_LOGGING = True
 BATCH_SIZE = 8
 
+# Potential fix if GPU problems, for now seems to work without
 # gpus = tf.config.experimental.list_physical_devices('GPU')
 # tf.config.experimental.set_memory_growth(gpus[0], True)
 
@@ -42,11 +49,11 @@ decode_map = {0: 0.5, 2: 1, 4: 1.5}
 def decode_sentiment(label):
     return decode_map[int(label)]
 
-def get_csv_as_df(dataset_name):
+def get_csv_as_df(dataset_name: str, columns: List[str]):
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     dataset_path = os.path.join(__location__, dataset_name)
     print("Open file:", dataset_path)
-    return pd.read_csv(dataset_path, encoding =DATASET_ENCODING , names=DATASET_COLUMNS)
+    return pd.read_csv(dataset_path, encoding =DATASET_ENCODING , names=columns)
 
 def get_tf_dataset(input_examples):
     """
@@ -98,34 +105,38 @@ def get_tf_dataset(input_examples):
     )
     return tf_dataset
 
-def TRAIN(model):
+def TRAIN(input_config: Config, output_config: Config):
+    """
+    input_config: pass None if starting from base model, otherwise pass config for model you want to fine tune
+    output_config: Pass config for resulting model after training completes
+    """
 
     print('Getting csv in pandas dataframe')
-    df = get_csv_as_df(DATASET_FILENAME)
+    df = get_csv_as_df(output_config.csv_file, output_config.dataset_columns)
     # remove unnecessary columns / cleanup text / map to our sentiment scores
     data_df = pd.DataFrame({
         'polarity': df['polarity'].apply(lambda x: float(x)+1),
         'text': df['text'].replace(r'\n', '', regex=True)
     })
 
-    print('Converting data into proper input format for training the model')
+    # Converting data into proper input format for training the model
     # InputExamples are fed to the tokenizer. 
     input_examples = data_df.apply(lambda x: InputExample(None, text_a=x['text'], text_b=None, label=x['polarity']), axis=1)
     train_input_examples, test_input_examples = train_test_split(input_examples, test_size=0.2)
 
-    print('Converting data to TensorFlow dataset objects')
+    # Converting data to TensorFlow dataset objects
     tf_train_dataset = get_tf_dataset(list(train_input_examples))
     tf_test_dataset = get_tf_dataset(list(test_input_examples))
 
-    print('Shuffling training data')
     train_data = tf_train_dataset.shuffle(50).batch(BATCH_SIZE).repeat(2)
-    print('Shuffling validation data')
     validation_data = tf_test_dataset.shuffle(10).batch(BATCH_SIZE).repeat(2)
 
     print('TRAINING MODEL')
-    if model == None:
-        print('NO MODEL PROVIDED, LOADING BASE MODEL')
+    if input_config == None:
+        print('NO INPUT CONFIG PROVIDED, LOADING BASE MODEL')
         model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+    else:
+        model = load_model(input_config.model_file)
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0), 
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
@@ -135,59 +146,41 @@ def TRAIN(model):
 
     print('SAVING MODEL')
     location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    path = os.path.join(location, MODEL_NAME)
+    path = os.path.join(location, output_config.model_file)
     model.save_pretrained(path, save_model=True)
 
 ### THIS IS FOR TESTING MODEL ###
 
-def load_model():
+def load_model(model_file: str):
     location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    new_model = TFBertForSequenceClassification.from_pretrained(os.path.join(location, MODEL_NAME))
+    new_model = TFBertForSequenceClassification.from_pretrained(os.path.join(location, model_file))
     new_model.summary()
     return new_model
 
-def make_prediction(model, tweet: str):
+def make_prediction(model, tweet: str, labels: List[int]):
     tf_batch = tokenizer(tweet, max_length=128, padding=True, truncation=True, return_tensors='tf')
     tf_outputs = model(tf_batch)
     tf_predictions = tf.nn.softmax(tf_outputs[0], axis=-1)
     tf_predictions = np.asarray(tf_predictions).tolist()
-    labels = [-1, 0, 1]
     if abs(tf_predictions[0][0] - tf_predictions[0][1]) < 0.00:
         return labels[1]
     elif tf_predictions[0][0] > tf_predictions[0][1]:
         return labels[0]
     else:
         return labels[2]
-    
-# def test_model(model):
-#     df = get_csv_as_df(VALIDATE_DATASET)
-#     data_df = pd.DataFrame({
-#         'label': df['polarity'].apply(lambda x: decode_sentiment(x)),
-#         'text': df['text'].replace(r'\n', '', regex=True)
-#     })
 
-#     input_examples = data_df.apply(lambda x: InputExample(None, text_a=x['text'], text_b=None, label=x['label']), axis=1)
-#     tf_dataset = get_tf_dataset(list(input_examples))
-
-#     # y = data_df.apply(lambda z: z['label'], axis=1)
-#     # # y = tf.convert_to_tensor(list(y))
-#     model.compile(
-#         optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0), 
-#         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
-#         metrics=[tf.keras.metrics.SparseCategoricalAccuracy('accuracy')]
-#     )
-
-#     loss, acc = model.evaluate(tf_dataset)
-#     print(f"Loss: {loss} | Accuracy: {acc}")
-
-def test_model(model):
-    df = get_csv_as_df(TEST_DATASET)
+def test_model(model, config: Config):
+    """
+    model: object returned from load_model()
+    config: Config for the file you are testing
+    """
+    df = get_csv_as_df(config.csv_file, config.dataset_columns)
     start_time = time.time()
     i = 0
     for index, row in df.iterrows():
         if index % 100 == 0:
             print(f"running {index}")
-        if row['polarity'] == make_prediction(model, row['text']):
+        if row['polarity'] == make_prediction(model, row['text'], config.test_labels):
             i += 1
-    print(f"TESTING TOOK {(time.time() - start_time) / 500} per tweet")
-    print(f"Accuracy: {i/500}")
+    print(f"TESTING TOOK {(time.time() - start_time) / config.csv_size} per tweet")
+    print(f"Accuracy: {i/config.csv_size}")
