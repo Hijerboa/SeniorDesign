c5a7303450ae79d99f7dd412ec1a500385c318f4
Nick Cleary <nicholascleary01@gmail.com>
2022-03-25 15:43:04 -0400

    Updates

--- app/initializer.py
+++ app/initializer.py
@@ -32,14 +32,14 @@ from tasks.machine_learning_tasks import keyword_extraction_by_bill
 session = create_session()
 bills: [Bill] = session.query(Bill).filter(Bill.keywords == None).all()
 for bill in bills:
-    keyword_extraction_by_bill.apply_async((bill.bill_id,))
+    keyword_extraction_by_bill.apply_async((bill.bill_id,))"""
 
 from db.database_connection import create_session
 from db.models import Bill
 from tasks.bill_request_tasks import run_process_bill_request
 
 session = create_session()
-bills: [Bill] = session.query(Bill).limit(10).all()
+bills: [Bill] = session.query(Bill).limit(1000).all()
 for bill in bills:
     run_process_bill_request(bill.bill_id, 1)
-session.close()"""
+session.close()
