1acb81033fc9551cbafb500d5644095d582212ec
Noxid00 <Noxid00@gmail.com>
2022-04-15 17:22:52 -0400

    smth

--- app/dashboard/dashboard2.py
+++ app/dashboard/dashboard2.py
@@ -12,16 +12,13 @@ from db import database_connection as conn
 from sqlalchemy.sql import func
 import db.models as models
 
-STYLE_BUTTON_CLOSED = 'fa bi-chevron-double-down mb-1 '
-STYLE_BUTTON_OPENED = 'bi bi-chevron-double-up mb-1 '
+STYLE_BUTTON_CLOSED = 'fa bi-chevron-double-down mb-1'
+STYLE_BUTTON_OPENED = 'bi bi-chevron-double-up mb-1'
 LOREM_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 # Somewhat cannibalized from the DBC documentation
 
-
-def _getNavbar(): # TODO: Change icon to somehting else, add twitter links, remove search bar, make responsive dropdown open alternate search.
-    LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
-
+def _makeSearchArea():
     # Bar for the search functionality. This will have a callback to update from the list of bills, etc.
     search_bar = dbc.Row(
         [
@@ -36,6 +33,10 @@ def _getNavbar(): # TODO: Change icon to somehting else, add twitter links, remo
         className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
         align="center",
     )
+    return search_bar
+
+def _getNavbar(searchArea):  # TODO: Change icon to somehting else, add twitter links, remove search bar, make responsive dropdown open alternate search, make navbar toggler switch at proper screen size.
+    LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
     # Overall navbar container
     navbar = dbc.Navbar(
@@ -60,10 +61,10 @@ def _getNavbar(): # TODO: Change icon to somehting else, add twitter links, remo
                 dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
                 # Collapse for mobile responsiveness
                 dbc.Collapse(
-                    search_bar,
+                    searchArea,
                     id="navbar-collapse",
                     is_open=False,
-                    navbar=True,
+                    navbar=False,
                 ),
             ],
             fluid=True
@@ -88,7 +89,9 @@ def _getBillSentimentChart():
 
 # Get the card for current bill information
 
-#TODO: Make this into a parent class to be instantiated for different card/agg types.
+# TODO: Make this into a parent class to be instantiated for different card/agg types.
+
+
 def _getBillInfoCard(i):
     parent_div = html.Div([
         dbc.Card([
@@ -102,21 +105,24 @@ def _getBillInfoCard(i):
             ]),
             dbc.Collapse([
                 html.Div(
-                    html.I(className='fa-solid fa-face-laugh text-success', style={'font-size':"8rem"}),
+                    html.I(className='fa-solid fa-face-laugh text-success p-3',
+                           style={'font-size': "8rem"}),
                     className='d-flex justify-content-center'
                 ),
 
                 dbc.CardBody([
                     html.B('Sample Bill Text:'),
                     html.P(LOREM_TEXT)
-                ])
+                ], className='pt-0')
             ], id='collapse-'+str(i), is_open=False)
-        ], color='dark', className='w-100')
+        ], className='w-100')
     ], className='d-flex align-items-center h-75'
     )
     return parent_div
 
 # TODO: Load data from the selected bill
+
+
 def _getBillSummary():
     parent_div = html.Div(
         dbc.Card([
@@ -136,23 +142,19 @@ def _getBillSummary():
                 html.A('Bill/Link/url', href='/'),
                 html.P('...')
             ])
-        ], className='h-100', color='dark'
+        ], className='h-100',
         ),
     )
     return parent_div
 
 
-def _getOffCanvas(): #TODO: Fill with bill search elements
+def _getOffCanvas(searchArea):  # TODO: Fill with bill search elements
     offcanvas = dbc.Offcanvas(
-            html.P(
-                "This is the content of the Offcanvas. "
-                "Close it by clicking on the close button, or "
-                "the backdrop."
-            ),
-            id="offcanvas",
-            title="Title",
-            is_open=False,
-        )
+        searchArea,
+        id="offcanvas",
+        title="Title",
+        is_open=False,
+    )
     return offcanvas
 
 # Server object for containerization
@@ -171,12 +173,14 @@ class Server:
         load_figure_template('vapor')
         # Create app context with stylesheets
         self.app = dash.Dash(__name__, external_stylesheets=[
-                             dbc.themes.VAPOR, dbc.icons.BOOTSTRAP, "https://use.fontawesome.com/releases/v6.1.1/css/all.css", dbc_css])
+                             dbc.themes.QUARTZ, dbc.icons.BOOTSTRAP, "https://use.fontawesome.com/releases/v6.1.1/css/all.css", dbc_css])
 
         # Build main app layout
+        searchArea = _makeSearchArea()
         self.app.layout = html.Div(children=[
-            _getNavbar(),  # Get the navbar elements and place into the layout above the main container
-            _getOffCanvas(), #Get the off canvas search area and place into the layout.
+            _getNavbar(searchArea),  # Get the navbar elements and place into the layout above the main container
+            # Get the off canvas search area and place into the layout.
+            _getOffCanvas(searchArea),
             dbc.Container([
                 dbc.Row([
                         dbc.Col(  # This column contains the bill information card
@@ -210,7 +214,8 @@ class Server:
         )
 
         # Register Card Collapses:
-        for i in range(1, 4): # TODO this will be dynamic based on the number/type of cards added.
+        # TODO this will be dynamic based on the number/type of cards added.
+        for i in range(1, 4):
             @self.app.callback(
                 Output("collapse-"+str(i), "is_open"),
                 Output("button-collapse-"+str(i), "className"),
